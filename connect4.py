3aef417e0d1a70e22fdd2b4833a86a78e1a5235e
Nam Nguyen <namnguyen1995@gmail.com>
2016-03-21 12:05:20 -0400

    Updated place_token

--- connect4.py
+++ connect4.py
@@ -1,34 +1,51 @@
 #!/usr/bin/env python
 
-class Connect4:	
+class Connect4:
 	row = 7
 	col = 7
 	win = 4
 	board = []
-	
+	winner = False
+
 	def __init__(self, row, col, win):
 		self.row = row
 		self.col = col
 		self.win = win
 		self.board = [[0 for x in xrange(col)] for x in xrange(row)]
-		
-	def printboard(self, board):
+
+
+	def print_board(self, board):
 		for row in board:
 			for e in row:
-			    print e,
-			print 
-	
-	def placetoken(self, col):
+				print e,
+			print
+
+
+	def place_token(self, col):
 		i = self.row - 1
-		while(i > 0):
-			if (self.board[i][col] == 0):
+		while i >= 0:
+			if self.board[i][col] == 0:
 				self.board[i][col] = 1
-				break
+				return 1
 			else:
-				i = i - 1
-		
-	def winner(self):
+				i -= 1
 		return 0
-x = Connect4(6, 6, 4)
-x.placetoken(2)
-x.printboard(x.board)
+
+
+	def winner(self):
+		pass
+
+
+	def main(self):
+		pass
+
+
+game = Connect4(7, 7 ,4)
+game.place_token(6)
+game.place_token(6)
+game.place_token(6)
+game.place_token(6)
+game.place_token(6)
+game.place_token(6)
+game.place_token(6)
+game.print_board(game.board)
